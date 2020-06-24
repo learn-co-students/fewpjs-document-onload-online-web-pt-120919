bc0472978c3cfe16dce1fda5a59350fe98bf2359
cep104 <candicelizabethpeters@gmail.com>
2020-06-24 18:54:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+// document.addEventListener('DOMContentLoaded', changeString);
+// function changeString(){
+// document.getElementById('text').innerHTML = 'This is really cool!';
+// }
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('text').innerHTML = 'This is really cool!';
+  });
