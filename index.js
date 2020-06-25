e501561cf5392addf244643afe548002bb6e0337
cep104 <candicelizabethpeters@gmail.com>
2020-06-24 19:02:06 -0500

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,16 @@
 // document.getElementById('text').innerHTML = 'This is really cool!';
 // }
 
+// document.addEventListener( "DOMContentLoaded", function () {
+//     updateDOM()
+//   } );
+  
+  
+//   function updateDOM() {
+//     document.getElementById( "text" )
+//       .innerHTML = "This is really cool!";
+//   }
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('text').innerHTML = 'This is really cool!';
   });
