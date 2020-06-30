88fe10289e1f098c772881d8619c76836a2cf267
dwanerich <dwanerich@gmail.com>
2020-06-30 19:55:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("The DOM has loaded");
+    
+
+    function updateText() {
+       
+        document.getElementById("text").textContent = "This is really cool!";
+    }
+    updateText();
+});
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
